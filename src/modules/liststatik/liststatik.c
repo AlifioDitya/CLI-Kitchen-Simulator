e1ed3cf771a65115bcb28c7d01488c58c4e6a44c
AlifioDitya <alifioditya@gmail.com>
2022-11-01 14:13:58 +0700

    Added liststatik

--- src/modules/liststatik/liststatik.c
+++ src/modules/liststatik/liststatik.c
@@ -32,13 +32,13 @@ IdxType getLastIdx(ListStatik l) {
 }; 
 
 /* ********** Test Indeks yang valid ********** */
-boolean isIdxValid(ListStatik l, IdxType i) {
+boolean isIdxListValid(ListStatik l, IdxType i) {
 /* Mengirimkan true jika i adalah indeks yang valid utk kapasitas List l */
 /* yaitu antara indeks yang terdefinisi utk container*/
     return ((i >= IDX_MIN)&&(i < CAPACITY));
 };
 
-boolean isIdxEff(ListStatik l, IdxType i) {
+boolean isIdxListEff(ListStatik l, IdxType i) {
 /* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
 /* yaitu antara 0..length(l)-1 */
     return ((i >= IDX_MIN)&&(i < listLength(l)));
@@ -46,13 +46,13 @@ boolean isIdxEff(ListStatik l, IdxType i) {
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test List kosong *** */
-boolean isEmpty(ListStatik l) {
+boolean isListEmpty(ListStatik l) {
 /* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
 /* *** Test List penuh *** */
     return listLength(l)==0 ;
 };
 
-boolean isFull(ListStatik l) {
+boolean isListFull(ListStatik l) {
 /* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
     return listLength(l)==CAPACITY;
 };
@@ -61,14 +61,14 @@ boolean isFull(ListStatik l) {
 /* ***  Perhatian : List boleh kosong!! *** */
 int indexOf(ListStatik l, int IDMakanan) {
 /* Search apakah ada elemen List l yang bernilai memiliki IDMakanan */
-/* Jika ada, menghasilkan indeks i terkecil, dengan ELMT(l,i) = val */
+/* Jika ada, menghasilkan indeks i terkecil, dengan Elmt(l,i) = val */
 /* Jika tidak ada atau jika l kosong, mengirimkan IDX_UNDEF */
 /* Skema Searching yang digunakan bebas */
     int i = 0 , len = listLength(l);
     boolean found = true ;  
     while (i<len && found)
     { 
-        if (ID(ELMT(l,i)) == IDMakanan)
+        if (ID(Elmt(l,i)) == IDMakanan)
         { 
             found = false;
         }
@@ -79,19 +79,19 @@ int indexOf(ListStatik l, int IDMakanan) {
 
 /* ********** MENAMBAH ELEMEN ********** */
 /* *** Menambahkan elemen terakhir *** */
-void insertFirst(ListStatik *l, ElType val) {
+void insertFirst(ListStatik *l, Makanan val) {
 /* Proses: Menambahkan val sebagai elemen pertama List */
 /* I.S. List l boleh kosong, tetapi tidak penuh */
 /* F.S. val adalah elemen pertama l yang baru */
 /* *** Menambahkan elemen pada index tertentu *** */
     int i;
     for (i=listLength(*l)-1; i >= 0; i--) {
-        ELMT(*l,i+1) = ELMT(*l,i);
+        Elmt(*l,i+1) = Elmt(*l,i);
     }
-    ELMT(*l, 0) = val;
+    Elmt(*l, 0) = val;
 }
 
-void insertAt(ListStatik *l, ElType val, IdxType idx) {
+void insertAt(ListStatik *l, Makanan val, IdxType idx) {
 // KAMUS
     int i;
 // ALGORITMA
@@ -100,19 +100,19 @@ void insertAt(ListStatik *l, ElType val, IdxType idx) {
 /* *** Menambahkan elemen terakhir *** */
 /* Proses: Menambahkan val sebagai elemen pada index idx List */
     for (i=listLength(*l)-1; i >= idx; i--) {
-        ELMT(*l,i+1) = ELMT(*l,i);
+        Elmt(*l,i+1) = Elmt(*l,i);
     }
-    ELMT(*l, idx) = val;   
+    Elmt(*l, idx) = val;   
 }
 
-void insertLast(ListStatik *l, ElType val) {
+void insertLast(ListStatik *l, Makanan val) {
 /* Proses: Menambahkan val sebagai elemen terakhir List */
 /* I.S. List l boleh kosong, tetapi tidak penuh */
 /* F.S. val adalah elemen terakhir l yang baru */
-    ELMT(*l, getLastIdx(*l)+1) = val;
+    Elmt(*l, getLastIdx(*l)+1) = val;
 }
 
-void deleteFirst(ListStatik *l, ElType *val) {
+void deleteFirst(ListStatik *l, Makanan *val) {
 /* Proses : Menghapus elemen pertama List */
 /* I.S. List tidak kosong */
 /* F.S. val adalah nilai elemen pertama l sebelum penghapusan, */
@@ -121,15 +121,15 @@ void deleteFirst(ListStatik *l, ElType *val) {
 // KAMUS
     int i;
 // ALGORITMA
-    *val = ELMT(*l, 0);
+    *val = Elmt(*l, 0);
     for (i=0; i < getLastIdx(*l); i++) {
-        ELMT(*l, i) = ELMT(*l, i+1);
+        Elmt(*l, i) = Elmt(*l, i+1);
     }
     Neff(*l)--;
 }
 
 /* *** Menghapus elemen pada index tertentu *** */
-void deleteAt(ListStatik *l, ElType *val, IdxType idx) {
+void deleteAt(ListStatik *l, Makanan *val, IdxType idx) {
 /* Proses : Menghapus elemen pada index idx List */
 /* I.S. List tidak kosong, idx adalah index yang valid di l */
 /* F.S. val adalah nilai elemen pada index idx l sebelum penghapusan, */
@@ -138,15 +138,15 @@ void deleteAt(ListStatik *l, ElType *val, IdxType idx) {
 // KAMUS
     int i;
 // ALGORITMA
-    *val = ELMT(*l, idx);
+    *val = Elmt(*l, idx);
     for (i=idx; i<getLastIdx(*l); i++) {
-        ELMT(*l,i) = ELMT(*l,i+1);
+        Elmt(*l,i) = Elmt(*l,i+1);
     }
     Neff(*l)--;
 }
 
 /* *** Menghapus elemen terakhir *** */
-void deleteLast(ListStatik *l, ElType *val) {
+void deleteLast(ListStatik *l, Makanan *val) {
 /* Proses : Menghapus elemen terakhir List */
 /* I.S. List tidak kosong */
 /* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
@@ -154,6 +154,29 @@ void deleteLast(ListStatik *l, ElType *val) {
 /*      List l mungkin menjadi kosong */
 // KAMUS
 // ALGORITMA
-    *val = ELMT(*l, listLength(*l)-1);
+    *val = Elmt(*l, listLength(*l)-1);
     Neff(*l)--;
 }
+
+/* Interaksi I/O */
+// void printList(ListStatik l){
+// /* Proses : Menuliskan isi List dengan traversal, List ditulis di antara kurung 
+//    siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+//    karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+// /* I.S. l boleh kosong */
+// /* F.S. Jika l tidak kosong: [e1,e2,...,en] */
+// /* Jika List kosong : menulis [] */
+// // KAMUS
+//     IdxType i;
+// // ALGORITMA
+//     printf("[");
+
+//     for (i=0; i<=getLastIdx(l); i++) {
+//         printString(Foo(ELMT(l,i)));
+//         if (i < getLastIdx(l)) {
+//         printf(",");
+//         }
+//     }
+
+//     printf("]");
+// }
